19dba83b62d970f10748e6e02f27ae87dbe4176f
samdev2200 <samuel2200@hotmail.com>
2025-02-26 19:07:34 +0100

    logs

--- samujava/src/main/java/es/samujava/Main.java
+++ samujava/src/main/java/es/samujava/Main.java
@@ -1,7 +1,11 @@
 package es.samujava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.error("Hello world!");
     }
 }
